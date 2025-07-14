ee7425e2144f9e30d0097fe53b9bbe30a383153c
Almaz Murzabekov <almaz@murzabekov.net>
2025-07-14 13:53:27 +0200

    feat: tauri-react interaction example

---
+++ nookat/src-tauri/src/entities/mod.rs
@@ -0,0 +1,3 @@
+pub mod system_info;
+
+pub use self::system_info::SystemInfo;
--- nookat/src-tauri/src/lib.rs
+++ nookat/src-tauri/src/lib.rs
@@ -1,14 +1,29 @@
+mod entities;
+
+use crate::entities::SystemInfo;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_system_info() -> Result<SystemInfo, String> {
+    println!("Getting system info");
+    Ok(SystemInfo {
+        cpu_usage: 10.0,
+        memory_usage: 20.0,
+        disk_usage: 30.0,
+    })
+}
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_system_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
